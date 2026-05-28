58177fd172bf607f7601ed9c76ef34f1549d696d
Roger Leal <roger.mo.leal@gmail.com>
2026-05-28 13:34:21 -0300

    feat(FEAT08): Criar o AuthInterceptor para injetar o id do usuário em requisições HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    CoreModule
-  ],
-  providers: [provideHttpClient()],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, RouterModule.forRoot(routes), CoreModule],
+  providers: [provideHttpClient(withInterceptors([authInterceptor]))],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
