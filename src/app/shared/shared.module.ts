860dda45940367a7b89f133253c7a3c0ee22a76a
Roger Leal <roger.mo.leal@gmail.com>
2026-05-29 13:09:27 -0300

    feat(FEAT12): Criar o FooterComponent reutilizável

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   // Componentes, pipes e diretivas reutilizáveis serão declarados e exportados aqui nas FEATs seguintes.
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
