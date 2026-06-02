25c0bac442d05ebc5ab2f5313da93a90ccc53bc2
Roger Leal <roger.mo.leal@gmail.com>
2026-06-02 14:09:20 -0300

    feat(FEAT16): Adicionar roteamento interno para telas de autenticação no AuthModule

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [LoginComponent, RegisterComponent, PasswordResetComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
