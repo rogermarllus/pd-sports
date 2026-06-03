484bfab2f87fe14150bdc105c2b3fc1c3a4697b3
Roger Leal <roger.mo.leal@gmail.com>
2026-06-03 15:30:27 -0300

    feat(FEAT20): Criar roteamento do HomeModule com importação do SharedModule

--- src/app/features/home/home.routing.module.ts
+++ src/app/features/home/home.routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { ModalitiesComponent } from './modalities/modalities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'modalidades', component: ModalitiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
