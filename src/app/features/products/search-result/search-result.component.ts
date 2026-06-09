c1a4e0c3998e06d387a4a11f81f6367bce29c804
Roger Leal <roger.mo.leal@gmail.com>
2026-06-09 13:41:46 -0300

    feat(FEAT23): Criar ProductsModule com roteamento interno e importação do SharedModule

---
+++ src/app/features/products/search-result/search-result.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result',
+  standalone: false,
+  templateUrl: './search-result.component.html',
+  styleUrl: './search-result.component.css',
+})
+export class SearchResultComponent {}
