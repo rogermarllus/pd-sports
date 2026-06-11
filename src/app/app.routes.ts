e5742accc635eb1040237dac916885f95302b8dd
Roger Leal <roger.mo.leal@gmail.com>
2026-06-11 16:25:17 -0300

    feat(FEAT33): Criar DashboardComponent com informações simuladas das vendas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -31,17 +31,18 @@ export const routes: Routes = [
         loadChildren: () => import('./features/user/user.module').then((m) => m.UserModule),
         canActivate: [authGuard],
       },
-      {
-        path: 'admin',
-        loadChildren: () => import('./features/admin/admin.module').then((m) => m.AdminModule),
-        canActivate: [authGuard, adminGuard],
-      },
     ],
   },
 
   {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.module').then((m) => m.AuthModule),
+  },
+
+  {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.module').then((m) => m.AdminModule),
+    canActivate: [authGuard, adminGuard],
   },
 
   {
